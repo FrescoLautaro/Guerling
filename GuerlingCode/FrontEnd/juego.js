6bacbb0e1d8c4e3f9fe11ead548f707a91ab1cc4
Dainobot <ignacio.martin.daino@gmail.com>
2023-09-27 12:36:21 -0300

    progreso en iniciar sesion y empezamos el juego

--- GuerlingCode/FrontEnd/juego.js
+++ GuerlingCode/FrontEnd/juego.js
@@ -13,3 +13,25 @@ function login()
         alert("Datos incorrectos, intente nuevamente.");
     }
 }
+function registrar()
+{
+    let user = document.getElementById("user").value;
+    let pass = document.getElementById("password").value;
+    window.location="index.html";
+        alert("Te registraste como: " + user);
+}
+
+let palabra = 'texto';
+let arrayPalabras = palabra.split('');
+let longitudFila = document.querySelector('.fila');
+
+arrayPalabras.forEach(() => {
+    longitudFila.innerHTML += " <input type='text' maxLenght='1' class='cuadrado' ";
+})
+
+let cuadrados = document.querySelectorAll(".cuadrado");
+cuadrados = [...cuadrados];
+
+let palabraUsuario = []
+
+/* cuadrados.forEach(Element => ) */
