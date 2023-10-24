e263024c59e8d369dc29f435f57bb5bacf34d050
Frescorp <fresco.lautaroet36@gmail.com>
2023-10-24 17:43:18 -0300

    Añadimos el backend a la pagina

---
+++ GuerlingCode/BackEnd/app.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
+
+const mongodbURI = process.env.MONGODB_URI; 
+const dbName = 'guerling';
+
+const client = new MongoClient(mongodbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+let db;
+
+// Mueve la lógica de conexión a una función asíncrona
+async function connectToDb() {
+    try {
+        await client.connect();
+        console.log("Conectado a la base de datos.");
+        db = client.db(dbName);
+    } catch (err) {
+        console.error('Error al conectar con MongoDB:', err);
+    }
+}
+
+async function init() {
+    await connectToDb();
+    
+    app.set("view engine", "ejs");  //Establece ejs como el motor de plantillas para renderizar las vistas.
+    app.set("views", __dirname + "/views"); //Define la ubicación del directorio de vistas. __dirname es una variable global en Node.js que obtiene el directorio del módulo actual.
+    
+    app.use(express.static(__dirname + '/../frontend'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(express.static(__dirname + '/public'));
+
+    app.get('/', (req, res) => { //cuando alguien acceda a la pagina inicial, se devolvera el index.html desde el servidor
+        res.sendFile(__dirname + '/../frontend/index.html');
+    });
+
+    app.get("/seleccionCategoria", (req, res) => {
+        res.render("seleccionCategoria");
+    });
+
+    app.get('/juego/:cat', (req, res) => { //estamos definiendo una ruta GET con un parámetro denominado :cat. Este parámetro es dinámico, lo que significa que puede tomar cualquier valor
+        const categoria = decodeURIComponent(req.params.cat);
+        console.log("Categoría seleccionada:", categoria);
+        getPalabraCat(categoria)
+            .then(doc => {
+                // Verifica si doc existe y si tiene una propiedad 'palabra'
+                if (!doc || !doc.palabra) {
+                    console.error('Documento no encontrado o no tiene propiedad "palabra":', doc);
+                    res.status(404).send('No se encontró un documento válido para la categoría seleccionada.');
+                    return;
+                }
+                res.render('jugar', { documento: doc }); //Si todo está en orden, utilizamos la función res.render para renderizar la vista del juego (jugar.ejs) y le pasamos el documento que contiene la palabra y su descripción
+            })
+            .catch(error => {
+                console.error('Error en la función principal:', error);
+                res.status(500).send('Hubo un error al procesar tu solicitud.');  // Añadir una respuesta al cliente
+            });
+    });
+
+    async function getPalabraCat(categoria) {
+        try {
+            const collection = db.collection(categoria);
+            const resp = await collection.aggregate([{ $sample: { size: 1 } }]).toArray();
+            
+            // Agrega esta línea para ver el resultado
+            console.log(resp);
+            
+            return resp[0];
+        } catch (error) {
+            console.error('Error al obtener los documentos:', error);
+            throw error;
+        }
+    }
+
+    app.listen(port, () => {
+        console.log(`Servidor corriendo en http://localhost:${port}`);
+    });
+}
+
+init();
--- GuerlingCode/FrontEnd/juego.js
+++ GuerlingCode/FrontEnd/juego.js
@@ -27,164 +27,162 @@ function registrar()
 
 /*Juego*/
 
-document.addEventListener("DOMContentLoaded", () => {
-    createSquares();
-    getNewWord();
-  
-    let guessedWords = [[]];
-    let availableSpace = 1;
-  
-    let word;
-    let guessedWordCount = 0;
-  
-    const keys = document.querySelectorAll(".keyboard-row button");
-  
-    function getNewWord() {
-      fetch(
-        `https://wordsapiv1.p.rapidapi.com/words/?random=true&lettersMin=5&lettersMax=5`,
-        {
-          method: "GET",
-          headers: {
-            "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-            "x-rapidapi-key": "<YOUR_KEY_GOES_HERE>",
-          },
-        }
-      )
-        .then((response) => {
-          return response.json();
-        })
-        .then((res) => {
-          word = res.word;
-        })
-        .catch((err) => {
-          console.error(err);
+
+let palabra = document.getElementById("palabra").innerText; //Obtiene la palabra que el jugador debe adivinar desde el HTML.
+let intentosRestantes = 6; //Establece el número total de intentos que el jugador tiene para adivinar la palabra.
+const filas = document.querySelectorAll('.row');  // Selecciona todas las filas del tablero de juego.
+const casillas = document.querySelectorAll('.square'); //Selecciona todas las casillas individuales del tablero de juego.
+const botonesTeclado = document.querySelectorAll(".filaTeclado button"); //Selecciona todos los botones del teclado virtual.
+const btnPista = document.getElementById('btnPista'); //Selecciona el botón que revela la pista.
+const textoPista = document.getElementById('textoPista'); //Selecciona el lugar donde se muestra la pista.
+let filaActual = 0; //Mantiene un registro de la fila en la que el jugador está actualmente escribiendo.
+
+
+//Funcion para configurar las casillas
+
+//Esta función pasa por todas las filas y casillas del juego.
+//Desactiva todas las casillas que no estén en la fila actual (para evitar que el jugador escriba en ellas).
+//Si una casilla está en la fila actual, se asegura de que esté vacía.
+function configurarCasillas() {
+    for (let i = 0; i < filas.length; i++) {
+        const casillasFila = filas[i].querySelectorAll('.square');
+        casillasFila.forEach(casilla => {
+            casilla.disabled = (i !== filaActual);
+            if (i === filaActual) {
+                casilla.value = '';
+            }
         });
     }
-  
-    function getCurrentWordArr() {
-      const numberOfGuessedWords = guessedWords.length;
-      return guessedWords[numberOfGuessedWords - 1];
-    }
-  
-    function updateGuessedWords(letter) {
-      const currentWordArr = getCurrentWordArr();
-  
-      if (currentWordArr && currentWordArr.length < 5) {
-        currentWordArr.push(letter);
-  
-        const availableSpaceEl = document.getElementById(String(availableSpace));
-  
-        availableSpace = availableSpace + 1;
-        availableSpaceEl.textContent = letter;
-      }
-    }
-  
-    function getTileColor(letter, index) {
-      const isCorrectLetter = word.includes(letter);
-  
-      if (!isCorrectLetter) {
-        return "rgb(58, 58, 60)";
-      }
-  
-      const letterInThatPosition = word.charAt(index);
-      const isCorrectPosition = letter === letterInThatPosition;
-  
-      if (isCorrectPosition) {
-        return "rgb(83, 141, 78)";
-      }
-  
-      return "rgb(181, 159, 59)";
-    }
-  
-    function handleSubmitWord() {
-      const currentWordArr = getCurrentWordArr();
-      if (currentWordArr.length !== 5) {
-        window.alert("Word must be 5 letters");
-      }
-  
-      const currentWord = currentWordArr.join("");
-  
-      fetch(`https://wordsapiv1.p.rapidapi.com/words/${currentWord}`, {
-        method: "GET",
-        headers: {
-          "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-          "x-rapidapi-key": "61c5e3986dmsh20c1bee95c2230dp18d1efjsn4668bbcfc1b3",
-        },
-      })
-        .then((res) => {
-          if (!res.ok) {
-            throw Error();
-          }
-  
-          const firstLetterId = guessedWordCount * 5 + 1;
-          const interval = 200;
-          currentWordArr.forEach((letter, index) => {
-            setTimeout(() => {
-              const tileColor = getTileColor(letter, index);
-  
-              const letterId = firstLetterId + index;
-              const letterEl = document.getElementById(letterId);
-              letterEl.classList.add("animate__flipInX");
-              letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
-            }, interval * index);
-          });
-  
-          guessedWordCount += 1;
-  
-          if (currentWord === word) {
-            window.alert("Congratulations!");
-          }
-  
-          if (guessedWords.length === 6) {
-            window.alert(`Sorry, you have no more guesses! The word is ${word}.`);
-          }
-  
-          guessedWords.push([]);
-        })
-        .catch(() => {
-          window.alert("Word is not recognised!");
-        });
-    }
-  
-    function createSquares() {
-      const gameBoard = document.getElementById("board");
-  
-      for (let index = 0; index < 30; index++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        square.classList.add("animate__animated");
-        square.setAttribute("id", index + 1);
-        gameBoard.appendChild(square);
-      }
-    }
-  
-    function handleDeleteLetter() {
-      const currentWordArr = getCurrentWordArr();
-      const removedLetter = currentWordArr.pop();
-  
-      guessedWords[guessedWords.length - 1] = currentWordArr;
-  
-      const lastLetterEl = document.getElementById(String(availableSpace - 1));
-  
-      lastLetterEl.textContent = "";
-      availableSpace = availableSpace - 1;
-    }
-  
-    for (let i = 0; i < keys.length; i++) {
-      keys[i].onclick = ({ target }) => {
-        const letter = target.getAttribute("data-key");
-  
-        if (letter === "enter") {
-          handleSubmitWord();
-          return;
+}
+
+// Llamar esta función al inicio para establecer el estado inicial
+configurarCasillas();
+
+
+// Función para borrar la última letra ingresada
+
+//Obtiene todas las casillas de la fila en la que está trabajando el jugador.
+//Comienza desde el final de la fila y se desplaza hacia atrás hasta encontrar la primera casilla que no esté vacía.
+//Borra el valor de esa casilla y la establece como el punto activo (focus) para futuras entradas.
+function borrarUltimaLetra() {
+    const casillasFila = filas[filaActual].querySelectorAll('.square');
+    for (let i = casillasFila.length - 1; i >= 0; i--) {
+        if (casillasFila[i].value) {
+            casillasFila[i].value = '';
+            casillasFila[i].focus();
+            return;
         }
-  
-        if (letter === "del") {
-          handleDeleteLetter();
-          return;
-        }
-  
-        updateGuessedWords(letter);
-      };
     }
-  });
+}
+
+
+// Función para manejar la entrada de letras
+
+//Toma una letra como argumento.
+//Pasa por todas las casillas de la fila en la que está trabajando el jugador.
+//Busca la primera casilla vacía y establece su valor como la letra dada.
+//Si esa casilla no es la última de la fila, mueve el focus a la siguiente casilla.
+function manejarLetra(letra) {
+    const casillasFila = filas[filaActual].querySelectorAll('.square');
+    for (let casilla of casillasFila) {
+        if (!casilla.value) {
+            casilla.value = letra;
+            return; // Sale de la función una vez que ingresa una letra
+        }
+    }
+}
+
+
+
+document.addEventListener('keyup', function(event) {
+    const key = event.key.toLowerCase();
+    if (key.length === 1 && key.match(/[a-zñ]/i)) {
+        manejarLetra(key);
+    } else if (key === "backspace" || key === "delete") {
+        borrarUltimaLetra();
+    } else if (key === "enter") {
+        revisarIntento();
+    }
+});
+
+//Funcion para manejar los intentos de adivinar del jugador
+
+//Obtiene el intento actual del jugador juntando todas las letras de la fila en la que está trabajando.
+//Comprueba cada letra del intento contra la palabra objetivo.
+//Colorea las casillas según si la letra está en la posición correcta, si la letra está en la palabra pero no en la posición correcta, o si la letra no está en la palabra en absoluto.
+//Si el jugador ha adivinado correctamente la palabra, o si ha utilizado todos sus intentos, muestra la descripción de la palabra. Si no, pasa al siguiente intento (fila).
+function revisarIntento() {
+    const casillasFila = filas[filaActual].querySelectorAll('.square');
+    const intento = [...casillasFila].map(casilla => casilla.value.toLowerCase()).join('');
+    let aciertos = 0;
+
+    for(let i = 0; i < palabra.length; i++) {
+        if(intento[i] === palabra[i]) {
+            aciertos++;
+            casillasFila[i].classList.add('verde');
+        } else if (palabra.includes(intento[i])) {
+            casillasFila[i].classList.add('amarillo');
+        } else {
+            casillasFila[i].classList.add('gris');
+        }
+    }
+
+    if (aciertos === palabra.length) {
+        mostrarVictoryModal();
+    } else if (intentosRestantes <= 0 || (filaActual === filas.length - 1 && aciertos !== palabra.length)) {
+        mostrarLoseModal();  // Mostrar el modal de perder
+    } else {
+        intentosRestantes--;
+        if (filaActual < filas.length - 1) {
+            filaActual++;
+            configurarCasillas(); // Asegurarse de que solo la fila actual esté habilitada
+        }
+    }
+}
+
+//Funcion para mostrar la descripcion de la palabra a adivinar
+//Muestra un modal con la descripción de la palabra que el jugador estaba tratando de adivinar.
+function mostrarVictoryModal() {
+    const modal = document.getElementById('victoryModal');
+    const descElement = document.getElementById("victoryDesc");
+    modal.style.display = "block";
+    descElement.innerText = document.getElementById("desc").innerText;
+}
+
+//Cierra el modal anterior.
+function closeVictoryModal() {
+    const modal = document.getElementById('victoryModal');
+    modal.style.display = "none";
+}
+
+function mostrarLoseModal() {
+    const modal = document.getElementById('loseModal');
+    modal.style.display = "block";
+}
+
+function closeLoseModal() {
+    const modal = document.getElementById('loseModal');
+    modal.style.display = "none";
+}
+
+// Event listeners
+botonesTeclado.forEach(btn => {
+    btn.addEventListener('click', function() {
+        const key = this.dataset.key;
+        if (key === 'del') {
+            borrarUltimaLetra();
+        } else if (key !== 'enter') {
+            manejarLetra(key);
+        } else {
+            revisarIntento();
+        }
+    });
+});
+
+
+
+
+
+btnPista.addEventListener('click', function() {
+    textoPista.style.display = textoPista.style.display === 'block' ? 'none' : 'block'; 
+});
